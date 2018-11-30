6470bb1929a5767af56abe014348317f6937841c
Klose6 <scsehebut@gmail.com>
2018-11-29 22:21:01 -0800

    Add initial recipe component and shopping component

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+
+export class RecipeListComponent implements OnInit {
+  currentNewRecipes: Recipe[] = [];
+
+  recipes: Recipe[] = [
+    new Recipe('Chili Grilled Beef', 'Grilled beef with chilis', 'assets/images/recipe_chili_grilled_beef.jpg'),
+    new Recipe('Shrimp Salad', 'Grilled shrimp with salad', 'assets/images/recipe_shrimp_salad.jpg'),
+    new Recipe('Chicken Flat', 'Steamed chicken with vegies', 'assets/images/recipe_chicken_flat.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addNewRecipe() {
+    // this.addNew = true;
+  }
+}
